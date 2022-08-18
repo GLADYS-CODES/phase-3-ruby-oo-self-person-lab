f82f66fabcef75e6380c5e0bbe8918b53ada777a
GLADYS <mungaiglado106@gmail.com>
2022-08-18 13:34:27 +0300

    commit tree c0bf515123d4fbafb8deaeb5a412234e2b6542fb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,10 @@
-# your code goes here
+class Person 
+    attr_accessor :bank_account
+    attr_reader :name, :hygiene, :happiness
+
+    def initialize (name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = value_checker (happiness)
+        @hygiene = value_checker (hygiene)
+    end
